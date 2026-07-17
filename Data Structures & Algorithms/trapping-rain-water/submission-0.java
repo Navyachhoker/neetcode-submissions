ccd9f497caacdb9a5edc86834954f328b18ed32b
Navya Chhoker <143236749+Navyachhoker@users.noreply.github.com>
2026-07-17 21:41:29 +0530

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int trap(int[] height) {
+
+        int n = height.length;
+
+        int[] maxl = new int[n];
+        int[] maxr = new int[n];
+
+        maxl[0] = height[0];
+        for(int i = 1; i < n; i++){
+            maxl[i] = Math.max(maxl[i - 1], height[i]);
+        }
+
+        maxr[n - 1] = height[n - 1];
+        for(int i = n - 2; i >= 0; i--){
+            maxr[i] = Math.max(maxr[i + 1], height[i]);
+        }
+
+        int water = 0;
+
+        for(int i = 0; i < n; i++){
+            water += Math.min(maxl[i], maxr[i]) - height[i];
+        }
+
+        return water;
+    }
+}
